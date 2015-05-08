aca7c744532282ddb74c8a74a4a7410c7a9c5c9e
Anton Kasyanov <antonk@remobjects.com>
2015-05-08 17:05:14 +0300

    Try to always resolve addresses as IPv4 first in TcpClient

--- Source/RemObjects.InternetPack/Dns.cs
+++ Source/RemObjects.InternetPack/Dns.cs
@@ -1,100 +1,94 @@
 /*---------------------------------------------------------------------------
-  RemObjects Internet Pack for .NET - Core Library
-  (c)opyright RemObjects Software, LLC. 2003-2012. All rights reserved.
-
-  Using this code requires a valid license of the RemObjects Internet Pack
-  which can be obtained at http://www.remobjects.com?ip.
+  RemObjects Internet Pack for .NET
+  (c)opyright RemObjects Software, LLC. 2003-2015. All rights reserved.
 ---------------------------------------------------------------------------*/
 
 using System;
 using System.Net;
+using System.Net.Sockets;
 
 namespace RemObjects.InternetPack.Dns
 {
-    public class DnsLookup
-    {
-        public static IPAddress ResolveFirst(String hostname)
-        {
-            IPAddress[] lAddresses = ResolveAll(hostname);
-            if (lAddresses.Length == 0)
-                throw new DnsResolveException(String.Format("Could not resolve HostName {0}", hostname));
+	public static class DnsLookup
+	{
+		public static IPAddress ResolveFirst(String hostname)
+		{
+			IPAddress[] lAddresses = DnsLookup.ResolveAll(hostname);
+			if (lAddresses.Length == 0)
+				throw new DnsResolveException("Could not resolve HostName {0}", hostname);
 
-            return lAddresses[0];
-        }
+			// Try to resolve as IPv4 first
+			for (Int32 i = 0; i < lAddresses.Length - 1; i++)
+			{
+				if (lAddresses[i].AddressFamily == AddressFamily.InterNetwork)
+					return lAddresses[i];
+			}
 
-        public static IPAddress ResolveRandom(String hostname)
-        {
-            IPAddress[] lAddresses = ResolveAll(hostname);
-            if (lAddresses.Length == 0)
-                throw new DnsResolveException(String.Format("Could not resolve hostname {0}", hostname));
+			return lAddresses[0];
+		}
 
-            Random lRandom = new Random();
-            return lAddresses[lRandom.Next(lAddresses.Length)];
-        }
+		public static IPAddress ResolveRandom(String hostname)
+		{
+			IPAddress[] lAddresses = ResolveAll(hostname);
+			if (lAddresses.Length == 0)
+				throw new DnsResolveException("Could not resolve hostname {0}", hostname);
 
-        public static IPAddress[] ResolveAll(String hostname)
-        {
-            IPAddress lAddress = TryStringAsIPAddress(hostname);
-            if (lAddress != null)
-                return new IPAddress[] { lAddress };
+			Random lRandom = new Random();
+			return lAddresses[lRandom.Next(lAddresses.Length)];
+		}
 
-            IPHostEntry lEntry = System.Net.Dns.GetHostEntry(hostname);
-            return lEntry.AddressList;
-        }
+		public static IPAddress[] ResolveAll(String hostname)
+		{
+			IPAddress lAddress = TryStringAsIPAddress(hostname);
+			if (lAddress != null)
+				return new IPAddress[] { lAddress };
 
-        public static IPAddress TryStringAsIPAddress(String hostname)
-        {
-            if (hostname.StartsWith("[")) // ipv6
-            {
-                try
-                {
-                    return IPAddress.Parse(hostname);
-                }
-                catch (FormatException)
-                {
-                    return null;
-                }
-            }
+			IPHostEntry lEntry = System.Net.Dns.GetHostEntry(hostname);
+			return lEntry.AddressList;
+		}
 
-            String[] lFields = hostname.Split('.');
-            if (lFields.Length != 4)
-                return null;
+		public static IPAddress TryStringAsIPAddress(String hostname)
+		{
+			if (hostname.StartsWith("[")) // ipv6
+			{
+				try
+				{
+					return IPAddress.Parse(hostname);
+				}
+				catch (FormatException)
+				{
+					return null;
+				}
+			}
 
-            for (Int32 i = 0; i < 4; i++)
-            {
-                if (lFields[i].Length > 3)
-                    return null;
+			String[] lFields = hostname.Split('.');
+			if (lFields.Length != 4)
+				return null;
 
-                for (Int32 j = 0; j < lFields[i].Length; j++)
-                    if (lFields[i][j] < '0' && lFields[i][j] > '9')
-                        return null;
-            }
-            try
-            {
-                return IPAddress.Parse(hostname);
-            }
-            catch (FormatException)
-            {
-                return null;
-            }
-        }
+			for (Int32 i = 0; i < 4; i++)
+			{
+				if (lFields[i].Length > 3)
+					return null;
 
-        public static String ReverseResolve(IPAddress address)
-        {
-            IPHostEntry lEntry = System.Net.Dns.GetHostEntry(address);
+				for (Int32 j = 0; j < lFields[i].Length; j++)
+					if (lFields[i][j] < '0' && lFields[i][j] > '9')
+						return null;
+			}
+			try
+			{
+				return IPAddress.Parse(hostname);
+			}
+			catch (FormatException)
+			{
+				return null;
+			}
+		}
 
-            return lEntry.HostName;
-        }
-    }
+		public static String ReverseResolve(IPAddress address)
+		{
+			IPHostEntry lEntry = System.Net.Dns.GetHostEntry(address);
 
-#if FULLFRAMEWORK
-    [System.Serializable()]
-#endif
-    public class DnsResolveException : Exception
-    {
-        public DnsResolveException(String message)
-            : base(message)
-        {
-        }
-    }
+			return lEntry.HostName;
+		}
+	}
 }
---
+++ Source/RemObjects.InternetPack/Exceptions/DnsResolveException.cs
@@ -0,0 +1,28 @@
+﻿/*---------------------------------------------------------------------------
+  RemObjects Internet Pack for .NET
+  (c)opyright RemObjects Software, LLC. 2003-2015. All rights reserved.
+
+  Using this code requires a valid license of the RemObjects Internet Pack
+  which can be obtained at http://www.remobjects.com
+---------------------------------------------------------------------------*/
+
+using System;
+
+namespace RemObjects.InternetPack.Dns
+{
+#if FULLFRAMEWORK
+	[System.Serializable()]
+#endif
+	public class DnsResolveException : Exception
+	{
+		public DnsResolveException(String message)
+			: base(message)
+		{
+		}
+
+		public DnsResolveException(String message, params Object[] args)
+			: this(String.Format(message, args))
+		{
+		}
+	}
+}
